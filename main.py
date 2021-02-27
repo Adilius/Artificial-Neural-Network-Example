c27a930c18c4d4c8417b3c590d767a30a6c116dd
Adil <leugue-gamer@hotmail.com>
2021-02-27 15:35:45 +0100

    Added function to create simple ANN model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-
 import_start = time.time()
+
 import os   #Turn off annoying tensorflow cuda error message
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #Turn off annoying tensorflow cuda error message
 from rich.console import Console    #Rich used for pretty console printing
@@ -16,6 +16,33 @@ print("Importing modules took:", round(import_end - import_start,2),"seconds.")
 
 RANDOM_STATE = 66 #Sets seed for splitting train and test set
 FRAC = 0.85 #Set fraction of train dataset
+EPOCHS = 5 #Number of epochs to train model
+
+#Create and train tensorflow model of a neural network
+def create_train_model(train_features):
+
+    #Create model object
+    model = tf.keras.models.Sequential()
+
+    #Add layer containing feature columns to model
+    feature_columns = []
+
+    for i in train_features.columns[:-1]:
+        feature_columns.append(tf.feature_column.numeric_column(i))
+    feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+    model.add(feature_layer)
+
+    #Add linear layer to model for simple linear regressor
+    model.add(tf.keras.layers.Dense(units=1, input_shape=(1,)))
+
+    #Compile layers into model for TensorFlow execution
+    model.compile(
+        optimizer='adam',
+        loss="mse",
+        metrics= ['accuracy']
+    )
+
+    return model
 
 #Initialize console object
 console = Console()
@@ -23,17 +50,15 @@ console = Console()
 #Read file into dataframe
 column_names = ['Sex','Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings']
 df = pd.read_csv("abalone.data", names=column_names)
-print(df.head(10))
 
-#LabelEncoder
-categorical_feature_mask = df.dtypes==object
-categorical_cols = df.columns[categorical_feature_mask].tolist()
+#Preprocessing with label encoder
+categorical_feature_mask = df.dtypes==object #Categorical mask
+categorical_cols = df.columns[categorical_feature_mask].tolist() #Get categories using mask
 le = preprocessing.LabelEncoder()
-df[categorical_cols] = df[categorical_cols].apply(lambda col:le.fit_transform(col))
-print(categorical_cols)
-print(df.head(10))
+df[categorical_cols] = df[categorical_cols].apply(lambda col:le.fit_transform(col)) #Apply mask
 
-
+#Normalize column-wise in dataset 
+df = (df-df.min())/(df.max()-df.min())
 
 #Split training set and testing set
 train_dataset = df.sample(frac=FRAC, random_state=RANDOM_STATE)
@@ -45,4 +70,5 @@ train_lables = train_features.pop('Rings')
 test_features = test_dataset.copy()
 test_lables = test_dataset.pop('Rings')
 
-print(tf.__version__)
+ann_model = create_train_model(train_features)
+#ann_model.fit(train_features,train_lables, epochs=EPOCHS)
